2c6ae418140be4bc960beec57a6be26f18c2d332
Manuel Kiessling <manuel@kiessling.net>
2011-11-01 14:05:17 +0100

    added AcmeDemoBundle:Thing entity

---
+++ src/Acme/DemoBundle/Entity/Thing.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Acme\DemoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Acme\DemoBundle\Entity\Thing
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Acme\DemoBundle\Entity\ThingRepository")
+ */
+class Thing
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $value
+     *
+     * @ORM\Column(name="value", type="string", length=255)
+     */
+    private $value;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
