c3a7bc1391cff88d33ad36ee2a4f72e8915d79ce
Manuel Kiessling <manuel@kiessling.net>
2011-11-01 13:56:08 +0100

    added and configured vendor scripts doctrine-migrations and DoctrineMigrationsBundle

--- app/autoload.php
+++ app/autoload.php
@@ -14,6 +14,8 @@ $loader->registerNamespaces(array(
     'Monolog'          => __DIR__.'/../vendor/monolog/src',
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
+    'Doctrine\\DBAL\\Migrations' => __DIR__.'/../vendor/doctrine-migrations/lib',
+    'Doctrine\\DBAL'             => __DIR__.'/../vendor/doctrine-dbal/lib',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
